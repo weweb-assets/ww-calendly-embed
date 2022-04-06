dfb1fcf427ae1b72c1908f9f749d4e5625d9dd77
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2022-04-06 23:30:41 +0200

    First commit �������

---
+++ src/wwElement.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ww-calendly-embed"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: { type: Object, required: true },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.ww-calendly-embed {
+}
+</style>
---
+++ ww-config.js
@@ -0,0 +1,8 @@
+export default {
+  editor: {
+    label: {
+      en: "Calendly embed",
+    },
+  },
+  properties: {},
+};
